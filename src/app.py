476e561777906166fc188671aa808cde8a9caf5a
Matias <matiassanchezabrego@gmail.com>
2025-03-14 12:11:52 -0300

    Retrieval information from web scraper

--- src/app.py
+++ src/app.py
@@ -1,8 +1,16 @@
 import streamlit as st
 from audio_recorder_streamlit import audio_recorder
 from langchain_openai import ChatOpenAI
-from config import settings
+from langchain_core.prompts import ChatPromptTemplate
 from langchain.schema import HumanMessage, SystemMessage, AIMessage
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores.faiss import FAISS
+from langchain.chains import create_retrieval_chain
+
+from config import settings
 import openai
 import base64
 
@@ -14,8 +22,12 @@ def transcribe_audio(audio_path):
         return transcript.text
 
 def get_ai_response(input_text):
-    response = llm_gpt([HumanMessage(content=input_text)])
-    return response.content
+
+    #response = llm_gpt([HumanMessage(content=input_text)])
+
+    response = retrieval_chain(input_text)
+
+    return response["answer"]
 
 def text_to_audio(client, text, audio_path):
     response = client.audio.speech.create(model="tts-1", voice="onyx", input=text)
@@ -27,6 +39,53 @@ def play_ai_response(audio_file):
     base64_audio=base64.b64encode(audio_bytes).decode("utf-8")
     audio_html=f'<audio src="data:audio/mp3;base64,{base64_audio}" controls autoplay>'
     st.markdown(audio_html, unsafe_allow_html=True)
+
+def get_documents_from_web(url):
+    loader = WebBaseLoader(url)
+    docs = loader.load()
+    
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 100,
+        chunk_overlap = 20
+    )
+    splitDocs = splitter.split_documents(docs)
+    return splitDocs
+
+def create_db(docs):
+    embedding = OpenAIEmbeddings()
+    vectorStore = FAISS.from_documents(docs, embedding = embedding)
+    return vectorStore
+
+def create_chain(vectorStore):
+    prompt = ChatPromptTemplate.from_template("""
+        Answer the user's question:
+        Context: {context}
+        Question: {input}
+        """)
+
+    chain = create_stuff_documents_chain(
+        llm=llm_gpt,
+        prompt=prompt
+
+    )
+
+    #retrieve info
+    # search_kwargs={"k":2} search for the 2 most relevant docs
+    retriever = vectorStore.as_retriever(search_kwargs={"k":2})
+    retrieval_chain = create_retrieval_chain(
+        retriever,
+        chain
+    )
+    return retrieval_chain
+
+def retrieval_chain(input_text):
+    docs = get_documents_from_web('https://atomchat.io/acerca-de-nosotros/')
+    vectorStore = create_db(docs)
+    chain = create_chain(vectorStore)
+    
+    return chain.invoke({
+        "input": input_text
+    })
 
 def main():
     st.title("Atom Voice Agent")
