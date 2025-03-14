aef14f4156bd4e5ad7bc4406003976c97fb725b0
Matias <matiassanchezabrego@gmail.com>
2025-03-13 21:23:05 -0300

    Adding whisper openai model, capable to translate audio to text

--- src/app.py
+++ src/app.py
@@ -3,8 +3,14 @@ from audio_recorder_streamlit import audio_recorder
 from langchain_openai import ChatOpenAI
 from config import settings
 from langchain.schema import HumanMessage, SystemMessage, AIMessage
+import openai
 
 llm_gpt = ChatOpenAI(model=settings.OPENAI_LLM_MODEL, temperature=settings.TEMPERATURE)
+
+def transcribe_audio(audio_path):
+    with open(audio_path, "rb") as audio_file:
+        transcript = openai.audio.transcriptions.create(model="whisper-1", file=audio_file)
+        return transcript.text
 
 def main():
     st.title("Atom Voice Agent")
@@ -14,9 +20,14 @@ def main():
     
     # Check if recording is done and available
     if recorded_audio:
+        openai.api_key = settings.OPENAI_API_KEY
         audio_file = settings.AUDIO_PATH+"audio_question.mp3"
         with open(audio_file, "wb") as f:
             f.write(recorded_audio)
+
+        transcribed_text = transcribe_audio(audio_file)
+
+        print(transcribed_text)
 
 if __name__ == "__main__":
     main()
--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import Optional
 from pydantic_settings import BaseSettings
 from dotenv import load_dotenv, find_dotenv
+import os 
 
 load_dotenv(find_dotenv(".env"))
 
@@ -9,6 +10,7 @@ root = Path(__file__).parent.parent
 
 class Settings(BaseSettings):
     OPENAI_LLM_MODEL: Optional[str] = "gpt-3.5-turbo-1106"
+    OPENAI_API_KEY: Optional[str] = os.getenv("OPENAI_API_KEY")
     LANGCHAIN_VERBOSE: bool = False
     TEMPERATURE: int = 0
 
